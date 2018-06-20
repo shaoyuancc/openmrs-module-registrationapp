ce4b740d3f3b6faa32b09db8822e5d17c0ad9b1c
shao yuan <shaoyuan_chewchia@college.harvard.edu>
2018-06-20 18:00:55 -0400

    Attempted to incorporate the global properties from haiticore and isanteplus modules into registration app, getting errors

--- api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppActivator.java
+++ api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppActivator.java
@@ -13,7 +13,6 @@
  */
 package org.openmrs.module.registrationapp;
 
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.PatientIdentifierType;
@@ -26,6 +25,18 @@ import org.openmrs.module.idgen.IdentifierSource;
 import org.openmrs.module.idgen.service.IdentifierSourceService;
 import org.openmrs.module.registrationcore.RegistrationCoreConstants;
 
+// CCSY ADDED
+import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.emrapi.utils.MetadataUtil;
+import org.openmrs.api.PatientService;
+import org.openmrs.GlobalProperty;
+import org.openmrs.module.registrationapp.metadata.bundles.HaitiPatientIdentifierTypeBundle;
+import org.openmrs.module.registrationapp.metadata.bundles.HaitiSedishMpiPatientIdentifierTypeBundle;
+import org.openmrs.module.registrationapp.metadata.bundles.RegistrationAppMetadataBundle;
+import org.openmrs.module.appframework.service.AppFrameworkService;
+import org.openmrs.ui.framework.resource.ResourceFactory;
+import org.openmrs.module.metadatadeploy.api.MetadataDeployService;
+
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
@@ -33,8 +44,32 @@ public class RegistrationAppActivator extends BaseModuleActivator {
 
     private static final Log log = LogFactory.getLog(RegistrationAppActivator.class);
 
-    @Override
+    /**
+     * @see ModuleActivator#willRefreshContext()
+     */
+    public void willRefreshContext() {
+        log.info("Refreshing Registration App Patient Dashboard Module");
+    }
+
+    /**
+     * @see ModuleActivator#contextRefreshed()
+     */
+    public void contextRefreshed() {
+        log.info("Registration App Patient Dashboard Module refreshed");
+    }
+
+    /**
+     * @see ModuleActivator#willStart()
+     */
+    public void willStart() {
+        log.info("Starting Registration App Patient Dashboard Module");
+    }
+
+    /**
+     * @see ModuleActivator#started()
+     */
     public void started() {
+
         setupIdentifierTypeGlobalProperties(Context.getAdministrationService(), Context.getService(IdentifierSourceService.class));
 
         super.started();
@@ -45,16 +80,82 @@ public class RegistrationAppActivator extends BaseModuleActivator {
         // identifier type from the EMR API module, if possible. (This may not be possible if things aren't configured right,
         // e.g. due to module startup order, in which case we log a warning and continue.)
         try {
+            AppFrameworkService appFrameworkService = Context.getService(AppFrameworkService.class);
+            PatientService patientService = Context.getPatientService();
+
             EmrApiProperties emrApiProperties = Context.getRegisteredComponents(EmrApiProperties.class).iterator().next();
             PatientIdentifierType primaryIdentifierType = emrApiProperties.getPrimaryIdentifierType();
 
             IdentifierSource sourceForPrimaryType = identifierSourceService.getAutoGenerationOption(primaryIdentifierType).getSource();
 
             administrationService.setGlobalProperty(RegistrationCoreConstants.GP_OPENMRS_IDENTIFIER_SOURCE_ID, sourceForPrimaryType.getId().toString());
+
+            // Setup for MPI Sedish Demo
+
+            log.info("Updating OpenMRS ID to iSantePlus ID");
+            changeOpenmrsIdName(patientService);
+
+            log.info("Installing Metadata Bundles");
+            installMetadataBundles();
+
+            //Disable the following apps
+            appFrameworkService.disableApp("referenceapplication.registrationapp.registerPatient");
+
         }
         catch (Exception ex) {
             log.warn("Failed to set global property for " + RegistrationCoreConstants.GP_OPENMRS_IDENTIFIER_SOURCE_ID + " based on " + EmrApiConstants.PRIMARY_IDENTIFIER_TYPE + ". Will try again at next module startup.", ex);
         }
+    }
+
+
+    /**
+     * Change the OpenMRD Id name to iSantePlus ID if it has already been loaded.
+     * @param patientService
+     */
+    public void changeOpenmrsIdName(PatientService patientService) throws Exception{
+
+        PatientIdentifierType openmrsIdType = patientService.getPatientIdentifierTypeByName("OpenMRS ID");
+        if (openmrsIdType != null) {
+            openmrsIdType.setName("iSantePlus ID");
+            patientService.savePatientIdentifierType(openmrsIdType);
+        }
+    }
+
+    /**
+     * Installs metadataBundles from multiple providers all found in the IsantePlusMetadataBundle.class
+     * Uses the MetadataDeploy Module
+     */
+    private void installMetadataBundles() throws Exception{
+
+        MetadataDeployService deployService = Context.getService(MetadataDeployService.class);
+
+        //Deploy metadata bundle if the ConfigurableGlobalProperties.METADATA_LAST_UPDATED_DATE is not equal to IsantePlusConstants.METADATA_LAST_UPDATED_DATE
+        String metadataLastUpdatedDate = Context.getAdministrationService().getGlobalProperty(ConfigurableGlobalProperties.METADATA_LAST_UPDATED_DATE);
+        if (!RegistrationAppConstants.METADATA_LAST_UPDATED_DATE.equals(metadataLastUpdatedDate)) {
+            log.info("installing registrationapp metadata bundle");
+            deployService.installBundle(Context.getRegisteredComponents(HaitiPatientIdentifierTypeBundle.class).get(0));
+            deployService.installBundle(Context.getRegisteredComponents(HaitiSedishMpiPatientIdentifierTypeBundle.class).get(0));
+            deployService.installBundle(Context.getRegisteredComponents(RegistrationAppMetadataBundle.class).get(0));
+        } else {
+            log.info("registrationapp metadata bundle not installed");
+        }
+
+
+    }
+
+    /**
+     * @see ModuleActivator#willStop()
+     */
+    public void willStop() {
+
+        log.info("Stopping Registration App Patient Dashboard Module");
+    }
+
+    /**
+     * @see ModuleActivator#stopped()
+     */
+    public void stopped() {
+        log.info("Registration App Patient Dashboard Module stopped");
     }
 
 }
--- api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppConstants.java
+++ api/src/main/java/org/openmrs/module/registrationapp/RegistrationAppConstants.java
@@ -4,4 +4,21 @@ public class RegistrationAppConstants {
 
     public static final String FIND_PATIENT_FRAGMENTS_EXTENSION_POINT = "registrationapp.findpatient.fragments";
 
+    // MPI Sedish Demo -> these were originally from ISantePlusConstants
+
+    public static final String METADATA_LAST_UPDATED_DATE = "20180620"; //Update this if you wish to trigger a new metadata load on an existing server
+
+    public static final String HaitiCore_ECID_UUID = "f54ed6b9-f5b9-4fd5-a588-8f7561a78401";
+
+    public static final String PATIENT_IDENTIFIER_TYPE_UUID_ISANTEPLUS_ID = "05a29f94-c0ed-11e2-94be-8c13b969e334"; //Note, this is the same as the OpenMRS ID identifier type because we rename it in the module activator
+
+    public static final String PATIENT_IDENTIFIER_TYPE_UUID_CODE_NATIONAL = "9fb4533d-4fd5-4276-875b-2ab41597f5dd";
+
+    public static final String PATIENT_IDENTIFIER_TYPE_UUID_CODE_ST = "d059f6d0-9e42-4760-8de1-8316b48bc5f1";
+
+    public static final String HaitiCore_BIOMETRIC_UUID = "e26ca279-8f57-44a5-9ed8-8cc16e90e559";
+
+    public static final String HaitiCore_BIOMETRIC_NATIONAL_UUID = "5a597bcc-26ad-11e8-b467-0ed5f89f718b";
+
+
 }
